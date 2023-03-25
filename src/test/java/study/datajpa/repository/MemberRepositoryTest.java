8401188a6b06ee292e91e0b79d437a6d1bc46de1
aka-nick <bechalrang@gmail.com>
2023-03-25 21:14:52 +0900

    섹션3(완) - MemberRepositoryTest.java 테스트 케이스 추가(spring data jpa 제공 메서드 활용)

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -24,4 +24,29 @@ class MemberRepositoryTest {
         assertThat(saved).isEqualTo(member);
     }
 
+    @Test
+    void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member by1 = memberRepository.findById(member1.getId()).get();
+        Member by2 = memberRepository.findById(member2.getId()).get();
+
+        // 단건 입력 & 조회 테스트
+        assertThat(member1).isEqualTo(by1);
+        assertThat(member2).isEqualTo(by2);
+
+        // 전체 조회 & 카운트 테스트
+        assertThat(memberRepository.findAll().size())
+                .isEqualTo(memberRepository.count());
+
+        // 삭제 테스트
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        assertThat(memberRepository.count()).isEqualTo(0);
+    }
+
 }
