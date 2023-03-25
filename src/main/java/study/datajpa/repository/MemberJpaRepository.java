544f6fc97c7415507ecf1e1f657651f10589148b
aka-nick <bechalrang@gmail.com>
2023-03-25 20:56:21 +0900

    섹션3 - MemberJpaRepository 전체 구성

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -1,5 +1,7 @@
 package study.datajpa.repository;
 
+import java.util.List;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
@@ -18,8 +20,26 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public long count() {
+        return em.createQuery("select count(m) from Member m ", Long.class)
+                .getSingleResult();
+    }
+
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(em.find(Member.class, id));
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m ", Member.class)
+                .getResultList();
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
     }
 
 }
