2853694aab2890e37fc67cb4bf44234764007407
aka-nick <bechalrang@gmail.com>
2023-03-27 20:30:24 +0900

    섹션5 - 페이징과 정렬(엔티티->DTO 매핑)

--- src/main/java/study/datajpa/controller/MemberApiController.java
+++ src/main/java/study/datajpa/controller/MemberApiController.java
@@ -2,9 +2,13 @@ package study.datajpa.controller;
 
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -15,9 +19,12 @@ public class MemberApiController {
     private final MemberRepository memberRepository;
 
     @PostConstruct
-    public void init() {
+    public void init() throws InterruptedException {
         Member member1 = new Member("member1");
         memberRepository.save(member1);
+        Thread.sleep(1000);
+        Member member2 = new Member("member2");
+        memberRepository.save(member2);
     }
 
 
@@ -30,5 +37,12 @@ public class MemberApiController {
     public String findMember2(@PathVariable("id") Member member) {
         return member.getUsername();
     }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable) {
+        Page<Member> pageEntity = memberRepository.findAll(pageable);
+        return pageEntity.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+    }
+
 
 }
